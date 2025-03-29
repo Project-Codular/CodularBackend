0658671cbe0db20524bc58252ef167a5d84a469d
Yuriy Magus <114507764+theMagusDev@users.noreply.github.com>
2025-03-29 13:07:41 +0300

    Add logging

--- cmd/codium-backend/main.go
+++ cmd/codium-backend/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"codium-backend/internal/config"
+	"codium-backend/lib/logger/handlers/slogpretty"
 	"log/slog"
+	"os"
 )
 
 const (
@@ -14,11 +16,54 @@ const (
 func main() {
 	cfg := config.MustLoad()
 
-	// setup logger
+	logger := setupLogger(cfg.Env)
+
+	logger.Info("Starting Codium backend", slog.String("env", cfg.Env))
+	logger.Debug("Debug messages are enabled")
 
 	// init storage
 
 	// init router
 
 	// start server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+	switch env {
+	case envLocal:
+		logger = setupPrettySlog(slog.LevelDebug)
+	case envDev:
+		logger = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{
+					Level: slog.LevelDebug,
+				},
+			),
+		)
+	case envProd:
+		logger = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{
+					Level: slog.LevelInfo,
+				},
+			),
+		)
+	}
+
+	return logger
+}
+
+func setupPrettySlog(level slog.Level) *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: level,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
