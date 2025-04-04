22daf669edea204b89e0923b2e73355adca1113d
Yuriy Magus <114507764+theMagusDev@users.noreply.github.com>
2025-04-04 19:40:00 +0300

    Add server start

--- cmd/codular-backend/main.go
+++ cmd/codular-backend/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"codium-backend/internal/config"
 	"codium-backend/lib/logger/handlers/slogpretty"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"log/slog"
+	"net/http"
 	"os"
 )
 
@@ -18,14 +21,33 @@ func main() {
 
 	logger := setupLogger(cfg.Env)
 
-	logger.Info("Starting Codium backend", slog.String("env", cfg.Env))
+	logger.Info("Starting Codular backend", slog.String("env", cfg.Env))
 	logger.Debug("Debug messages are enabled")
 
-	// init storage
+	// todo init storage
 
-	// init router
+	router := chi.NewRouter()
 
-	// start server
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	logger.Info("starting server", slog.String("address", cfg.HTTPServer.Address))
+
+	server := &http.Server{
+		Addr:         cfg.HTTPServer.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		logger.Error("failed to start server")
+	}
+
+	logger.Error("server stopped")
 }
 
 func setupLogger(env string) *slog.Logger {
